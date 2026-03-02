36a54004001f21225b5d16a9d773163f96189311
Thaksigan23 <thaksithaksigan@gmail.com>
2026-03-02 22:11:20 +0530

    fix index.html root

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Services from "./components/Services"
-import Stats from "./components/Stats"
-import Gallery from "./components/Gallery"
-import Testimonials from "./components/Testimonials"
-import Instagram from "./components/Instagram"
-import Map from "./components/Map"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
-import ScrollProgress from "./components/ScrollProgress"
-import Whatsapp from "./components/Whatsapp"
-
 function App() {
   return (
-    <>
-      <ScrollProgress />
-      <Navbar />
-      <Hero />
-      <Services />
-      <Stats />
-      <Gallery />
-      <Testimonials />
-      <Instagram />
-      <Map />
-      <Contact />
-      <Footer />
-      <Whatsapp />
-    </>
+    <div style={{padding:"40px", fontSize:"40px"}}>
+      Website is working
+    </div>
   )
 }
 
